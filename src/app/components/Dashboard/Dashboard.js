c1f22b4e1f4a13e23b83cc798776cb311d5b37ba
Benjamin Brown <bcb88616@uga.edu>
2024-04-02 23:55:20 -0400

    added form

--- src/app/components/Dashboard/Dashboard.js
+++ src/app/components/Dashboard/Dashboard.js
@@ -1,5 +1,9 @@
+"use client";
+
 import React from 'react';
+import { useState } from 'react';
 import ReminderList from './ReminderList';
+import AddReminder from './AddReminder';
 import './Dashboard.css'; // Import Dashboard-specific styles
 
 const reminders = [
@@ -8,10 +12,18 @@ const reminders = [
 ];
 
 const Dashboard = () => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const clickHandler = () => {
+    setIsClicked(true);
+  }
+
   return (
-    <div className="dashboard-container">
-      <button className="addReminderBtn">+ Add Reminder</button>
-      <ReminderList reminders={reminders} />
+    <div className="outer-container">
+      <div className={ isClicked ? "dashboard-container-clicked" : "dashboard-container"}>
+        <button className="addReminderBtn" onClick={clickHandler}>+ Add Reminder</button>
+        <ReminderList reminders={reminders} />
+      </div>
     </div>
   );
 }
