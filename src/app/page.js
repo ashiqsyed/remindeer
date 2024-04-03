e60a5c176e6410e3a5b081484564bcdf2aaa65cf
Benjamin Brown <bcb88616@uga.edu>
2024-04-03 00:26:44 -0400

    added form

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,6 @@
 import LoginPage from "./components/LoginPage"
+import Dashboard from "./components/Dashboard/Dashboard"
+import AddReminder from "./components/Dashboard/AddReminder"
 import "./globals.css"
 export default function Home() {
   return (
