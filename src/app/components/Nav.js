b23f6c9dcc763fa5f635e1d7a545395906b7d81d
ashiqsyed <ahs57816@uga.edu>
2024-04-03 13:46:58 -0400

    finished styling navbar

--- src/app/components/Nav.js
+++ src/app/components/Nav.js
@@ -2,16 +2,18 @@ import React from 'react'
 //import Link from 'next/Link'  *for ze links
 import "./Nav.css"
 // import remindeerLogo from "../../../public/Remindeer_SVG.svg"
-import logo from "../../../public/Remindeer_SVG.svg";
-
+import logo from "../../app/Remindeer_PNG.png"
+///Users/ashiqsyed/School/csci-4300/cs4300-project/remindeer/src/app/Remindeer_PNG.png
 
 const Nav = () => {
+
+    console.log(logo.src);
     return (
         <div className="navBar">
 
             <div className="nav-left">
                 <ul>
-                    <li><img src={logo} alt="Logo of Remindeer" width="100px"/></li>
+                    <li><img src={logo.src} alt="Logo of Remindeer" width="100px"/></li>
                     <li>Remindeer</li>
                 </ul>
             </div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 import LoginPage from "./components/LoginPage"
+import Dashboard from "./components/Dashboard/Dashboard.js"
 import Nav from "./components/Nav"
 import "./globals.css"
 export default function Home() {
   return (
     <div className="app-container">
-      <Nav />
-
       <LoginPage />
+      
     </div>
     
   );
