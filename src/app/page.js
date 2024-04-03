0701d32afa74d0e4c3e41860d4487843bd3cfe39
ashiqsyed <ahs57816@uga.edu>
2024-04-03 13:17:54 -0400

    fixing conflicts?

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,6 @@
 import LoginPage from "./components/LoginPage"
-<<<<<<< HEAD
 import Dashboard from "./components/Dashboard/Dashboard"
 import AddReminder from "./components/Dashboard/AddReminder"
-=======
-import Dashboard from "./components/Dashboard/Dashboard";
->>>>>>> 6d0a54b831452c90dd93a53b132330b3d0c6381d
 import "./globals.css"
 export default function Home() {
   return (
