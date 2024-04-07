edf538aa6beacdd58018f3cfaea3a4c213014025
ashiqsyed <ahs57816@uga.edu>
2024-04-07 17:07:06 -0400

    added delete and edit buttons, need to add implementation once we learn more about server routes. We also need to implement dynamic routes for viewing individual reminders when we learn more about server routes and making requests to the backend/database.

--- src/app/components/reminder/Reminder.js
+++ src/app/components/reminder/Reminder.js
@@ -12,6 +12,13 @@ const Reminder = ({ title, time, description, image }) => {
         <p>{time}</p>
         <p>{description}</p>
       </div>
+      {/* Edit and Delete buttons for CRUD operation, add implementation once we learn about backend and rendering individual reminders by 
+      making server and database requests. We need to add implementation for viewing individual reminders when we learn about requesting info
+      from the backend*/}
+      <div className={styles.buttonContainer}>
+        <button className={styles.editButton}>Edit</button>
+        <button className={styles.deleteButton}>Delete</button>
+      </div>
       
       
     </div>
