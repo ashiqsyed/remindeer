9da2eaa3df985103c2f6ee0d7cc9facb150211f8
ashiqsyed <ahs57816@uga.edu>
2024-04-07 00:26:49 -0400

    modified demo screen/path to use a demo component that is the same thing as the dashboard but the add button does not do anything

--- src/app/demo/page.js
+++ src/app/demo/page.js
@@ -2,11 +2,14 @@ import LoginPage from "../components/login-page/LoginPage"
 import Dashboard from "../components/Dashboard/Dashboard.js"
 import Nav from "../components/nav/Nav"
 import AddReminder from "../components/addreminder/AddReminder"
+import Demo from "../components/demo-screen/Demo.js"
 
 export default function Home() {
+    
     return (
         <div>
-            <Dashboard />
+            {/* <Dashboard /> */}
+            <Demo />
         </div>
 
     )
