197611bc15806b60d914ca4cb668cb9757d7cbf0
ashiqsyed <ahs57816@uga.edu>
2024-04-19 00:36:59 -0400

    verified that routes work in postman. The database is being updated accordingly based on the route

--- server/routes/api/remindeers.js
+++ server/routes/api/remindeers.js
@@ -6,40 +6,42 @@ const Remindeer = require('../../models/Remindeer');
 
 
 //gets all the remindeers
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
     Remindeer.find()
-        .then( (remindeers) => {
-            res.json(remindeers);
-        })
-        .catch( (err) => { res.status(404).json( {noitemsfound: 'Could not find items'})});
-});
+    .then((remindeers) => res.json(remindeers))
+    .catch((err) => res.status(404).json("No remindeers found"));
+})
 
 //gets the specific remindeer by id
 router.get('/:id', (req, res) => {
     Remindeer.findById(req.params.id)
-        .then( (remindeer) => res.json(remindeer))
-        .catch( (err) => res.status(404).json( {noitemfound: 'No Remindeer Found!'}));
+    .then( (remindeer) => res.json(remindeer))
+    .catch( (err) => res.status(404).json( {noitemfound: 'No Remindeer Found!'}));
+    // res.json("Testing get/:id server route");
 });
 
 //updates a remindeer by ID
-router.put('/:id', (req, res) => {
+router.put('/:id', bodyParser.json(), (req, res) => {
     Remindeer.findByIdAndUpdate( req.params.id, req.body)
-        .then( (remindeer) => res.json( {msg: "Item updated successfully"}))
-        .catch( (err) => res.status(400).json( {error: 'Unable to update database'})); 
+    .then( (remindeer) => res.json( {msg: "Item updated successfully"}))
+    .catch( (err) => res.status(400).json( {error: 'Unable to update database'})); 
+    // res.json("testing put/:id server route")
 });
 
 //creates a Remindeer
 router.post('/', bodyParser.json(), (req, res) => {
     Remindeer.create(req.body)
-        .then( (remindeer) => res.json( {msg: 'Remindeer added successfully!'}))
-        .catch( (err) => res.status(400).json( {error: "Error"}));
+    .then( (remindeer) => res.json( {msg: 'Remindeer added successfully!'}))
+    .catch( (err) => res.status(400).json( {error: "Error"}));
+    // res.json("testing post/ server route")
 });
 
 //deletes a Remindeer
 router.delete('/:id', (req, res) => {
     Remindeer.findByIdAndDelete(req.params.id)
-        .then( (remindeer) => res.json( {msg: "Remindeer deleted successfully"}))
+    .then( (remindeer) => res.json( {msg: "Remindeer deleted successfully"}))
+    // res.json("testing delete/:id server route")
 })
 
 
-module.export = router;
+module.exports = router;
