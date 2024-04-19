48222e4e883f4f275f75b5d218c75dcbe3c27b71
DanielJHan <118767272+DanielJHan@users.noreply.github.com>
2024-04-19 00:13:42 -0400

    Updated user routes

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -5,10 +5,18 @@ var bodyParser = require("body-parser");
 
 const User = require('../../models/User');
 
+//getting users
+router.get('/', (req, res) => {
+    User.find()
+        .then( (users) => res.json(users))
+        .catch( (err) => res.status(404).json( {noitemsfound: "No users found"}));
+});
 
-
-
-
+router.post('/', bodyParser.json(), (req, res) => {
+    User.create(req.body)
+        .then( (user) => res.json( {msg: 'User added successfully!'}))
+        .catch( (err) => res.status(400).json( {error: "Error"}));
+});
 
 
 module.exports = router;
