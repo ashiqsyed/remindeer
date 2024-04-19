fec8c64f2dfbde33492c74e9b078a3c5b4da670d
DanielJHan <118767272+DanielJHan@users.noreply.github.com>
2024-04-18 23:52:45 -0400

    Added routes for remindeers and framework

---
+++ server/routes/api/remindeers.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+var bodyParser = require("body-parser");
+
+const Remindeer = require('../../models/Remindeer');
+
+
+//gets all the remindeers
+router.get('/', (req, res) => {
+    Remindeer.find()
+        .then( (remindeers) => {
+            res.json(remindeers);
+        })
+        .catch( (err) => { res.status(404).json( {noitemsfound: 'Could not find items'})});
+});
+
+//gets the specific remindeer by id
+router.get('/:id', (req, res) => {
+    Remindeer.findById(req.params.id)
+        .then( (remindeer) => res.json(remindeer))
+        .catch( (err) => res.status(404).json( {noitemfound: 'No Remindeer Found!'}));
+});
+
+//updates a remindeer by ID
+router.put('/:id', (req, res) => {
+    Remindeer.findByIdAndUpdate( req.params.id, req.body)
+        .then( (remindeer) => res.json( {msg: "Item updated successfully"}))
+        .catch( (err) => res.status(400).json( {error: 'Unable to update database'})); 
+});
+
+//creates a Remindeer
+router.post('/', bodyParser.json(), (req, res) => {
+    Remindeer.create(req.body)
+        .then( (remindeer) => res.json( {msg: 'Remindeer added successfully!'}))
+        .catch( (err) => res.status(400).json( {error: "Error"}));
+});
+
+//deletes a Remindeer
+router.delete('/:id', (req, res) => {
+    Remindeer.findByIdAndDelete(req.params.id)
+        .then( (remindeer) => res.json( {msg: "Remindeer deleted successfully"}))
+})
+
+
+module.export = router;
